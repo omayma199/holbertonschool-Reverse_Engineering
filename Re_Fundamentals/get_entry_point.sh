1724651a8386602657c8b46c5f922456e6310dd5
omayma19 <5752@holbertonstudents.com>
2025-01-13 16:26:12 +0100

    test

--- Re_Fundamentals/get_entry_point.sh
+++ Re_Fundamentals/get_entry_point.sh
@@ -17,14 +17,14 @@ fi
 
 # Extract ELF header information using readelf
 magic_number=$(readelf -h "$file_name" | grep "Magic:" | awk '{$1=""; print $0}')
-class_format=$(readelf -h "$file_name" | grep "Class:" | awk '{print $2}')
-
+class=$(readelf -h "$file_name" | grep "Class:" | awk '{print $2}')
+byte_order=$(readelf -h "$file_name" | grep "Data:" | awk -F, '{print $2}' | xargs)
 entry_point_address=$(readelf -h "$file_name" | grep "Entry point address:" | awk '{print $NF}')
 
 # Display the information
 echo "ELF Header Information for '$file_name':"
 echo "----------------------------------------"
-echo "Type of Magic Number: $magic_number"
-echo "Class or Format: $class_format"
+echo "Magic Number: $magic_number"
+echo "Class: $class"
 echo "Byte Order: $byte_order"
 echo "Entry Point Address: $entry_point_address"
